be25b4c3c0fa7570d35d7afc77e99a7e9c8d90c2
Saurabh Mathur <saurabhmathur96@gmail.com>
2015-01-05 17:46:03 +0530

    Update rss-search.py

--- rss-search.py
+++ rss-search.py
@@ -1,16 +1,18 @@
 """
-rss-search.py: an rss feed filter
+news.py: an rss feed aggregator/filter
 """
 from feedparser import parse
 from operator import or_
 from urllib import urlretrieve
-from os import mkdir
+from os import mkdir, path
 from time import strftime, localtime
 from re import search
 
+LIM = 3
+
 def get_list_from_string(s):
     """ returns a list of lines """
-    return s.strip().split('\n')
+    return s.strip().splitlines()
 
 def get_text_from_feed_entry(entry):
     """ returns a dict from entry containing text and link """
@@ -18,13 +20,22 @@ def get_text_from_feed_entry(entry):
     
 def matches_word(text, word):
     """ checks if text matches word """
-    return search(r'\b'+word.lower()+r'\b', text.lower())
+    if search(r'\b'+word.lower()+r'\b', text.lower()):
+        return True
+    else:
+        return False
 
 
 def matches_any_word(text, keywords):
     """ checks if text matches any of the keywords """
     return reduce(or_, [matches_word(text, word) for word in keywords])
     
+def make_filename(text, words=LIM ,extension=None):
+    """ forms filename from text """
+    filename = '-'.join(text.split(' ')[:LIM])
+    if extension:
+        filename += extension
+    return filename
 
 def main(urls, keywords):
     """ main function """
@@ -32,22 +43,32 @@ def main(urls, keywords):
     entries = sum([raw_data.entries for raw_data in raw_data_list], [])
 
     required_data =  [get_text_from_feed_entry(entry) for entry in entries]
-
-    print len(required_data)
-
+    
+    print 'searching {} entries...'.format(len(required_data))
     results = filter(
                 lambda data: matches_any_word(data['text'], keywords), 
                 required_data
               )
 
+
     if results:
 
         dirname = strftime('%d-%m-%y', localtime())
-        mkdir(dirname)
-
+        if not path.isdir(dirname):
+            mkdir(dirname)
+                    
+        RESULT_COUNT = len(results)
+        
+        print '{} result(s) found'.format(RESULT_COUNT)
+        
         for i, result in enumerate(results):
-            filename = dirname + '/' + str(i)+'.html'
+            filename = path.join(
+                        dirname,
+                        make_filename(result['text'], extension = '.html')
+                       )
+            print 'downloading {} of {}...'.format(i+1, RESULT_COUNT),
             urlretrieve(result['link'], filename )
+            print 'done'
 
     else:
         print 'no results found.\n try again with more keywords and urls'
@@ -61,3 +82,5 @@ if __name__ == '__main__':
 
     with open('keywords.txt') as f:
         KEY_WORDS = get_list_from_string(f.read())
+    
+    main(URLS, KEY_WORDS)
